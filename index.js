6ffe61c8a69c828ef58aa3a0a3d8426ca5ccf457
Dante Doig-Acuna <doig.acuna@gmail.com>
2018-08-23 22:34:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.addEventListener('keydown', (event) => {
     // 180px
     let previousLeft = element.style.left
     //180
-    let previousLeftNumber = parseInt(previousLeft.replace ('px',''),10)
+    let previousLeftNumber = parseInt(previousLeft.replace('px',''),10)
     let newLeftNumber = (previousLeftNumber - 15) + "px"
     element.style.left = newLeftNumber    
   }
