e6670409eadda9491cd495bea7625223ffd39a6a
Dante Doig-Acuna <doig.acuna@gmail.com>
2018-08-23 22:26:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ document.addEeventListener('keydown', (event)=> {
   if (event.which === 37) {
     // 180px
     let previousLeft = element.style.previousLeft//18-
-    let previousLeftNumber = parstInt(previousLeft.replae('px',''),10)
-    let newLeftNumber = (previousLeftNumber -1)
-    
+    let previousLeftNumber = parstInt(previousLeft.replace ('px',''),10)
+    let newLeftNumber = (previousLeftNumber - 1) + "px"
+    element.style.left = newLeftNumber    
   }
 } 
 
