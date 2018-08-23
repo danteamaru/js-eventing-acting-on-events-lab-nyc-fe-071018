9acf868860d4d30e1644c0719819080b4a112299
Dante Doig-Acuna <doig.acuna@gmail.com>
2018-08-23 22:48:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ document.addEventListener('keydown', (event) => {
   function moveDodgerRight() {
     document.addEventListener('keydown', (event) => {
     if (event.which === 39) {
-    // 180px
-    let previousRight = element.style.left
-    //180
-    let previousRightNumber = parseInt(previousRight.replace('px',''), 10)
-    if (previousRightNumber < 400) {
-    let newRightNumber = (previousRightNumber + 15) + "px"
-    element.style.left = newRightNumber    
+      // 180px
+      let previousRight = element.style.left
+      //180
+      let previousRightNumber = parseInt(previousRight.replace('px',''), 10)
+      if (previousRightNumber < 400) {
+        let newRightNumber = (previousRightNumber + 15) + "px"
+      element.style.left = newRightNumber    
+      }
     }
-  }
 })
 moveDodgerRight();
